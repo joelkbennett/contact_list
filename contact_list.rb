7f303d57de00de6d5cbb70b0b8025ecb95288ea9
joelkbennett <joelkbennett@gmail.com>
2016-02-09 00:51:00 +0000

    Make utility executable

--- contact_list.rb
+++ contact_list.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby 
+
 require_relative 'contact'
 require 'byebug'
 
@@ -6,7 +8,7 @@ class ContactList
 
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
   def initialize
-    show_menu if ARGV.empty?
+    return show_menu if ARGV.empty?
     @command = ARGV[0]
     @arg = ARGV[1] unless ARGV[1] == nil
     check_command
@@ -16,11 +18,11 @@ private
 
   # Print a list of available commands
   def show_menu
-    menu = "\nHere is a list of available Commands
+    menu = "\n Available Commands:
              new\t- Create a new contact
              list\t- List all contacts
              show\t- Show a contact
-             search\t- Search contacts"
+             search\t- Search contacts\n\n"
     puts menu
   end
 
@@ -31,7 +33,9 @@ private
     when 'new' then add_contact
     when 'find' then get_contact # TODO: Input check
     when 'search' then search_contacts
-    else puts 'Command not recognized'
+    else 
+      puts "\n Command not recognized"
+      show_menu
     end
   end
 
@@ -44,45 +48,50 @@ private
   # Waits for user input and creates a new contact record
   # TODO: Add better validation
   def add_contact
-    puts "Enter contact name:"
+    puts " Enter contact name:"
     name = STDIN.gets.chomp
-    puts "Enter contact email:"
+    puts " Enter contact email:"
     email = STDIN.gets.chomp
     if Contact.uniq_email?(email)
       Contact.create(name, email)
-      puts "#{name} successfully added to contact list"
+      puts " #{name} successfully added to contact list"
     else
-      puts "#{email} already exists! Contact not added"
+      puts " #{email} already exists! Contact not added"
     end
   end
 
   # Takes ID and display contact if it exists
   def get_contact
     contact = Contact.find(@arg)
-    puts contact.nil? ? "Contact not found" : "#{contact[1]} (#{contact[2]})"
+    puts contact.nil? ? " Contact not found" : " #{contact[1]} (#{contact[2]})"
   end
 
   # Takes user input and searches contact list. Outputs all unique entries
   def search_contacts
     contacts = Contact.search(@arg.downcase).uniq
-    display_contacts(contacts)
+    display_contacts(contacts, false)
   end
 
   # Takes an array of contacts and formats the output
   def display_contacts(contacts, paginated)
-    contacts.each { |contact| puts "#{contact[0]}: #{contact[1]} (#{contact[2]})" }
-    puts "---\n#{contacts.size} records total" unless paginated
+    contacts.each { |contact| puts " #{contact[0]}: #{contact[1]} (#{contact[2]})" }
+    puts "---\n #{contacts.size} records total\n\n" unless paginated
   end
 
   # Take an array of contacts and paginates through them; returns nil
   def paginate(contacts, num_per_page)
+    system "clear"
     pages = contacts.each_slice(num_per_page)
     pages.each_with_index do |page, i| 
-      puts "Page #{i + 1} of #{pages.count}"
+      puts " Page #{i + 1} of #{pages.count}"
       display_contacts(page, true)
-      puts "\nhit enter to continue"
-      STDIN.gets.chomp
+      wait_for_enter
     end
+  end
+
+  def wait_for_enter
+    puts "\n hit enter to continue"
+    STDIN.gets.chomp
   end
 end
 
