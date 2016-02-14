0d6fe8aac8842369d23e38dd45b656d306f7ba7e
joelkbennett <joelkbennett@gmail.com>
2016-02-14 18:31:35 +0000

    Change Contact#all to read headers; output hash

--- contact_list.rb
+++ contact_list.rb
@@ -2,10 +2,10 @@
 
 require_relative 'contact'
 
-# Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
 
-  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+  PAGINATE = 5
+
   def initialize
     return show_menu if ARGV.empty?
     @command = ARGV[0]
@@ -41,28 +41,24 @@ private
   # Prints all contacts to the console -- I'd prefer to call this all_contacts
   def show_contacts
     contacts = Contact.all
-    contacts.size <= 5 ? display_contacts(contacts) : paginate(contacts, 5)
+    contacts.size <= 5 ? display_contacts(contacts, false) : paginate(contacts, PAGINATE)
   end
 
   # Waits for user input and creates a new contact record
-  # TODO: Add better validation
-  # TODO: Fuck this repl - replace with ARGS
   def add_contact
     puts "> Enter contact name:"
     name = STDIN.gets.chomp
     puts "> Enter contact email:"
     email = STDIN.gets.chomp
     if Contact.uniq_email?(email)
-      # TODO: I'm not trilled about the location of this; MOVE IT
       phone_nums = []
       puts "> Would you like to add a phone number? Y/N"
       add_num = STDIN.gets.chomp.downcase
       while add_num == 'y' do
-        phone_nums << add_phone_number
+        (phone_nums << add_phone_number)
         puts "> Add another? Y/N"
         add_num = STDIN.gets.chomp
       end
-
       Contact.create(name, email, phone_nums)
       puts "> #{name} successfully added to contact list"
     else
@@ -82,30 +78,84 @@ private
     display_contacts(contacts, false)
   end
 
-  # Takes an array of contacts and formats the output
+  # Takes an array of contacts and a Boolean if paginated and formats the output
   def display_contacts(contacts, paginated)
-    contacts.each { |contact| puts " #{contact[0]}: #{contact[1]} (#{contact[2]}), (#{contact[3]})" }
+    system('clear')
+    contacts.each do |contact| 
+      contact_str = " %{id}: %{name} (%{email}), (%{phone})"
+      puts contact_str % contact
+    end
     puts "---\n #{contacts.size} records total\n\n" unless paginated
   end
 
   def add_phone_number
     puts "> Phone Type (Home/Work/Mobile)"
-    type = STDIN.gets.chomp
+    type = STDIN.gets.chomp.to_sym
     puts "> Phone Number"
     number = STDIN.gets.chomp
-    [type, number]
+    {type => number}
   end
 
   # Take an array of contacts and paginates through them; returns nil
   def paginate(contacts, num_per_page)
     system "clear"
-    pages = contacts.each_slice(num_per_page)
-    pages.each_with_index do |page, i| 
-      puts "> Page #{i + 1} of #{pages.count}\n\n"
-      display_contacts(page, true)
-      wait_for_enter
+    pages = contacts.each_slice(num_per_page).to_a
+
+    # # TODO: Future feature - Add forward and back navigation
+    last_page = pages.count
+    current_page = 0
+    
+    puts "> Page #{current_page} of #{last_page}\n\n"
+
+    # Display the first page
+    page = pages[0]
+    display_contacts(page, true)
+
+    # Wait for initial input; Only go forward
+    puts"\n> Hit N for next page anything else to quit"
+    page_input = STDIN.gets.chomp
+
+    while page_input.downcase != 'q'
+      # Display the previous page
+      if page_input.downcase == 'p'
+        current_page -= 1
+        page = pages[current_page]
+        puts "> Page #{current_page} of #{pages.count}\n\n"
+        display_contacts(page, true)
+        puts"\n> Hit N for next page, P for previous page or anything else to quit"
+        page_input = STDIN.gets.chomp
+      # Display the next page on all other input
+      elsif page_input.downcase == 'n'
+        current_page += 1
+        page = pages[current_page]
+        puts "> Page #{current_page} of #{pages.count}\n\n"
+        display_contacts(page, true)
+        puts"\n> Hit N for next page, P for previous page  or anything else to quit"
+        page_input = STDIN.gets.chomp
+      end
     end
+
+    if page_input.downcase == 'q'
+      show_exit_message
+    end
+      
+    # pages.each_with_index do |page, i| 
+    #   puts "> Page #{i + 1} of #{pages.count}\n\n"
+    #   display_contacts(page, true)
+    #   wait_for_enter
+    # end
   end
+
+  def show_exit_message
+    puts "\n> Bye!\n\n"
+  end
+
+  # TODO: Finish with forward and back pagination
+  # def page_forward(page_num)
+  # end
+
+  # def page_backward(page_num)
+  # end
 
   # Waits for input from from the user before continuing; returns the input
   def wait_for_enter
